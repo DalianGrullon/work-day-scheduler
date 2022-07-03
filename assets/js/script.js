1d13691ccb30366741b1faf01c8f698af623fb69
Dalian Grullon <dalian.business@aol.com>
2022-07-03 03:48:14 -0400

    added local storage dynamic update based on user interactivity

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,18 +18,41 @@ function getCurrDay() {
 }
 
 function textAreaColor() {
-    let currHour = moment().hour();
-  
-    for (let i = 0; i < textAreas.length; i++) {
-      if (currHour == textAreas[i].attr("id")) {
-        textAreas[i].addClass("present");
-      } else if (currHour > textAreas[i].attr("id")) {
-        textAreas[i].addClass("past");
-      } else {
-        textAreas[i].addClass("future");
-      }
+  let currHour = moment().hour();
+
+  for (let i = 0; i < textAreas.length; i++) {
+    if (currHour == textAreas[i].attr("id")) {
+      textAreas[i].addClass("present");
+    } else if (currHour > textAreas[i].attr("id")) {
+      textAreas[i].addClass("past");
+    } else {
+      textAreas[i].addClass("future");
     }
   }
+}
+
+function storageCommit(textArea, hour) {
+  let eventData = textArea.val().trim();
+  localStorage.setItem(hour, eventData);
+
+  if (eventData === "") {
+    localStorage.removeItem(hour);
+  }
+}
+
+function storageDisplay() {
+  for (let i = 0; i < textAreas.length; i++) {
+    let storageKey = textAreas[i].siblings("div").text();
+    textAreas[i].text(localStorage.getItem(storageKey));
+  }
+}
 
 getCurrDay();
 textAreaColor();
+storageDisplay();
+
+$(".saveBtn").click(function() {
+    let siblingEl = $(this).siblings("textarea");
+    let siblingElHour = $(this).siblings("div").text();
+    storageCommit(siblingEl, siblingElHour);
+});
