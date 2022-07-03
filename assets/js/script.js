07c80485c570d546da0bd058deebea00aa2cb42b
Dalian Grullon <dalian.business@aol.com>
2022-07-03 03:46:26 -0400

    added text area color changing functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,35 @@
 let currentDayEl = $("#currentDay");
+let textAreaEl = $("textarea");
+let textAreas = [
+  $("#9"),
+  $("#10"),
+  $("#11"),
+  $("#12"),
+  $("#13"),
+  $("#14"),
+  $("#15"),
+  $("#16"),
+  $("#17"),
+];
 
 function getCurrDay() {
   let currDay = moment().format("dddd, MMMM Do");
   currentDayEl.text(currDay);
 }
 
+function textAreaColor() {
+    let currHour = moment().hour();
+  
+    for (let i = 0; i < textAreas.length; i++) {
+      if (currHour == textAreas[i].attr("id")) {
+        textAreas[i].addClass("present");
+      } else if (currHour > textAreas[i].attr("id")) {
+        textAreas[i].addClass("past");
+      } else {
+        textAreas[i].addClass("future");
+      }
+    }
+  }
+
 getCurrDay();
+textAreaColor();
